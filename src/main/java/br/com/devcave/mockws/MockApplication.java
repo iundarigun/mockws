0b65cbcefc8dc05535873915c8328a18131ea038
Oriol Canalias <oriol.canalias@wavy.global>
2019-10-09 15:31:06 -0300

    mockdefinition

--- src/main/java/br/com/devcave/mockws/MockApplication.java
+++ src/main/java/br/com/devcave/mockws/MockApplication.java
@@ -5,10 +5,13 @@ import br.com.devcave.mockws.filter.MockFilter;
 import br.com.devcave.mockws.utils.FileUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -17,6 +20,9 @@ import java.util.Arrays;
 @Slf4j
 @SpringBootApplication
 public class MockApplication {
+
+    @Value("${mock.definition-path:}")
+    private String definitionPath;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -27,7 +33,8 @@ public class MockApplication {
 
     @PostConstruct
     public void fillMock() throws IOException {
-        String definitions = FileUtils.readFromFile("classpath:", "mockdefinitions.json");
+        val path = StringUtils.isEmpty(definitionPath)?"classpath:":definitionPath;
+        String definitions = FileUtils.readFromFile(path, "mockdefinitions.json");
         MockResponse[] mockResponseList = objectMapper.readValue(definitions, MockResponse[].class);
         MockFilter.mockResponseList.addAll(Arrays.asList(mockResponseList));
     }
